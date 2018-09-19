798baaa202f90560801523aab18f872201d069dc
veb3 <vincent.balbarin@yale.edu>
2018-09-19 13:23:34 -0400

    Fixed indents. Modified Logging

--- HttpTrigger/main.py
+++ HttpTrigger/main.py
@@ -18,17 +18,27 @@ AUTH_CODE = env.get('AUTH_CODE')
 FW_IP = env.get('FW_IP')
 API_KEY = env.get('API_KEY')
 
+logging.basicConfig()
+
 def temp_auth_code():
-  return secrets.token_urlsafe(32)
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
+  auth_code = secrets.token_urlsafe(32)
+  logger.info('Generated auth_code {}'.format(auth_code))
+  return auth_code
 
 def primary_private_ip(ip_configs):
   """ This function extracts primary, private ipaddress """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   return [ip['properties']['privateIPAddress']
     for ip in ip_configs if ip['properties']['primary']][0]
 
 def palo_alto_tags(azure_dict, namespace='', delim='_'):
   """This function takes a dictionary of MS Azure tags and returns  a list
   of Palo Alto Networks formatted tags, takes an optional namespace for prefix for pan tag element"""
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   prefix = 'azure'
   if namespace:
     prefix = prefix + delim + namespace
@@ -42,6 +52,8 @@ def palo_alto_tags(azure_dict, namespace='', delim='_'):
 
 def azure_nic(post_req_data):
   """This function takes a JSON payload from the webhook and emits a dictionary of {"ip": []}"""
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   try:
     nic = json.loads(post_req_data['properties']['responseBody'])
     return {
@@ -52,29 +64,33 @@ def azure_nic(post_req_data):
     return {"ipAddress": None, "tags": None}
 
 def pan_firewall(hostname='', api_key=''):
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   try:
     fw = pan_fw.Firewall(hostname=hostname, api_key=api_key)
     version, platform, serial = parse(
       "SystemInfo(version='{}', platform='{}', serial='{}')",
       str(fw.refresh_system_info())).fixed
-    logging.info('FW version={}:FW platform={}: FW serial={}'.format(version, platform, serial))
+    logger.info('FW version={}:FW platform={}: FW serial={}'.format(version, platform, serial))
     return fw
   except Exception as e:
-    logging.error('Cannot connect to PAN:{}'.format(str(e)))
+    logger.error('Cannot connect to PAN:{}'.format(str(e)))
     return None
 
 def pan_tags(pan_fw, tag_names=[]):
   """ Returns a list of PAN tag objects. It will take a list of tag names and add them to exising tags """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   current_tags = pan_objs.Tag.refreshall(pan_fw, add=False)
   current_tag_names= [t.name for t in current_tags]
   new_tags = list(
     set(tag_names).difference(set(current_tag_names)))
-  logging.info('Supplied tags:{}'.format(tag_names))
+  logger.info('Supplied tags:{}'.format(tag_names))
   new_pan_tags = [pan_objs.Tag(name=t) for t in new_tags]
   # Add current and new tag objects to pan_fw object
   if new_pan_tags:
-    logging.info('Found {:d}'.format(len(new_tags)))
-    logging.info('Adding tags:{}'.format(new_tags))
+    logger.info('Found {:d}'.format(len(new_tags)))
+    logger.info('Adding tags:{}'.format(new_tags))
     tags_to_be_added = current_tags + new_pan_tags
     for pan_tag in tags_to_be_added:
       pan_fw.add(pan_tag)
@@ -82,12 +98,14 @@ def pan_tags(pan_fw, tag_names=[]):
     #tags_to_be_added[0].apply_similar()
     return tags_to_be_added
   else:
-    logging.info('All supplied tags already exist; no new tags added')
+    logger.info('All supplied tags already exist; no new tags added')
     return current_tags
 
 def pan_ips(pan_fw, azure_nic={"ipAddress": "", "tags": []}):
   """ Returns a list of PAN Address objects """
-  logging.info('IP address value: {}'.format(azure_nic['ipAddress']))
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
+  logger.info('IP address value: {}'.format(azure_nic['ipAddress']))
   current_ips = pan_objs.AddressObject.refreshall(pan_fw, add=False)
   for ip in current_ips: pan_fw.add(ip) # Re-add existing
   if azure_nic['ipAddress']:
@@ -96,7 +114,7 @@ def pan_ips(pan_fw, azure_nic={"ipAddress": "", "tags": []}):
       value=azure_nic['ipAddress'],
       tag=azure_nic['tags'])
       # Adds or updates ip object in pan_fw
-    logging.info('Adding az_ip')
+    logger.info('Adding az_ip')
     pan_fw.add(az_ip)
     az_ip.create()
     az_ip.apply()
@@ -106,6 +124,8 @@ def pan_ips(pan_fw, azure_nic={"ipAddress": "", "tags": []}):
 
 def azure_securityzone(azure_nic={"ipAddress": "", "tags": []}):
   """ Returns security zone from azure nic ipaddress """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   prefix = 'azure_SecurityZone'
   security_zone = [t for t in azure_nic['tags'] if '_SecurityZone_' in t]
   if security_zone:
@@ -119,6 +139,8 @@ def azure_securityzone(azure_nic={"ipAddress": "", "tags": []}):
 
 def pan_addressgroup_memberships(pan_fw, ip_name=None):
   """ Returns a list current addressgroup memberships """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   if ip_name == None:
     return []
   current_addressgroups = pan_objs.AddressGroup.refreshall(pan_fw, add=False)
@@ -126,6 +148,8 @@ def pan_addressgroup_memberships(pan_fw, ip_name=None):
 
 def pan_securityzone(pan_fw, ip_name=None, label='_SecurityZone_'):
   """ Returns security zone of a PAN AddressObject with ip_name """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   try:
     if ip_name:
       return [securityzone for securityzone in pan_addressgroup_memberships(pan_fw, ip_name=ip_name)
@@ -137,6 +161,8 @@ def pan_securityzone(pan_fw, ip_name=None, label='_SecurityZone_'):
 
 def pan_addressgroup_members(pan_fw, name=''):
   """ Returns a list of addressgroup members """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   try:
     if name:
       return [ao.static_value for ao in pan_objs.AddressGroup.refreshall(pan_fw, add=False)
@@ -146,6 +172,8 @@ def pan_addressgroup_members(pan_fw, name=''):
 
 def pan_addressgroup(pan_fw, address_group_name='', ip_address=None):
   """ Returns a PAN static IP address group object """
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
   current_ips = pan_objs.AddressObject.refreshall(pan_fw, add=False)
   ip_address_names = [ip.name for ip in current_ips if ip.value == ip_address]
   if not ip_address_names:
@@ -157,7 +185,7 @@ def pan_addressgroup(pan_fw, address_group_name='', ip_address=None):
   if not current_addressgroup:
     logging.warn('PAN AddressGroup {} does not exist'.format(address_group_name))
     return None
-  logging.info('Found PAN AddressGroup {}'.format(address_group_name))
+  logger.info('Found PAN AddressGroup {}'.format(address_group_name))
   current_addressgroup_static_values = set(current_addressgroup[0].static_value)
   addressgroup = pan_objs.AddressGroup(
     name=address_group_name,
@@ -167,21 +195,20 @@ def pan_addressgroup(pan_fw, address_group_name='', ip_address=None):
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
   """ Azure Function App Execution Loop """
-  logging.info('Python HTTP trigger function processed a request.')
-
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
+  logger.info('Python HTTP trigger function processed a request.')
   if AUTH_CODE is None:
     auth_code = temp_auth_code()
-    logging.info('AUTH_CODE environment variable not found.')
-    logging.info(f'Setting AUTH_CODE = {auth_code}')
+    logger.info('AUTH_CODE environment variable not found.')
+    logger.info(f'Setting AUTH_CODE = {auth_code}')
     environ['AUTH_CODE'] = auth_code
     return func.HttpResponse(
       body=f'A new authorization code generated. Please record this value; it will be available only at this time: {auth_code}',
       status_code=200)
-  
-  logging.info('AUTH_CODE environment variable found.')
-
+  logger.info('AUTH_CODE environment variable found.')
   code = req.params.get('code')
-  logging.info('Checking for valid auth code supplied in query string.')
+  logger.info('Checking for valid auth code supplied in query string.')
   # TODO: check for code in req.header.
   if not code:
     return func.HttpResponse(
@@ -191,38 +218,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
       body='Authorization code is invalid',
       status_code=401)
-
-  logging.info('Code supplied on query string matches AUTH_CODE.')
-
+  logger.info('Code supplied on query string matches AUTH_CODE.')
   fw = pan_firewall(hostname=FW_IP, api_key=API_KEY)
-  
   try:
     req_body = req.get_json()
     nic = azure_nic(req_body)
     tags = pan_tags(pan_fw=fw, tag_names=nic['tags'])
     if tags:
-      logging.info('Updating PAN Tags')
+      logger.info('Updating PAN Tags')
       tags[0].create_similar()
       tags[0].apply_similar()
-      logging.info('Updated PAN Tags')
+      logger.info('Updated PAN Tags')
     ips = pan_ips(pan_fw=fw, azure_nic=nic)
     if ips:
-      logging.info('Updating PAN AddressObjects')
-      for ip in ips: logging.info(ip.name)
-      logging.info('Updated PAN AddressObjects')
+      logger.info('Updating PAN AddressObjects')
+      for ip in ips: logger.info(ip.name)
+      logger.info('Updated PAN AddressObjects')
     az_ip_securityzone = azure_securityzone(azure_nic=nic)
-    logging.info('Security Zone for Azure {}: {}'.format(nic['ipAddress'], az_ip_securityzone))
+    logger.info('Security Zone for Azure {}: {}'.format(nic['ipAddress'], az_ip_securityzone))
     pan_ip_securityzone = pan_securityzone(pan_fw=fw, ip_name='ip_' + nic['ipAddress'])
-    logging.info('Security Zone for PAN {}: {}'.format('ip_' + nic['ipAddress'], pan_ip_securityzone))
+    logger.info('Security Zone for PAN {}: {}'.format('ip_' + nic['ipAddress'], pan_ip_securityzone))
     if pan_ip_securityzone:
       if pan_ip_securityzone != az_ip_securityzone:
-        logging.info('Removing {} from {}'.format('ip_' + nic['ipAddress'], pan_ip_securityzone))
+        logger.info('Removing {} from {}'.format('ip_' + nic['ipAddress'], pan_ip_securityzone))
         static_value_members = pan_addressgroup_members(pan_fw=fw, name=pan_ip_securityzone)
-        logging.info('Current static_value: {}'.format(static_value_members))
+        logger.info('Current static_value: {}'.format(static_value_members))
         s = set(static_value_members)
         s.discard('ip_' + nic['ipAddress'])
         new_static_value_members = list(s)
-        logging.info('New static_value: {}'.format(new_static_value_members))
+        logger.info('New static_value: {}'.format(new_static_value_members))
         former_addressgrp = pan_objs.AddressGroup(
           name=pan_ip_securityzone,
           static_value=new_static_value_members)
@@ -234,7 +258,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         fw.add(new_addressgrp)
         addressgrps = [former_addressgrp] + [new_addressgrp]
       else:
-        logging.info('No changes in AddressGroups.')
+        logger.info('No changes in AddressGroups.')
         addressgrps = []
     else:
       new_addressgrp = pan_addressgroup(
@@ -244,11 +268,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
       fw.add(new_addressgrp)
       addressgrps = [new_addressgrp]
     if addressgrps:
-      logging.info('Updating PAN AddressGroups: {}'.format(addressgrps))
+      logger.info('Updating PAN AddressGroups: {}'.format(addressgrps))
       for ag in addressgrps:
         ag.create()
         ag.apply()
-        logging.info('Updated PAN AddressGroup: {}'.format(ag.name))
+        logger.info('Updated PAN AddressGroup: {}'.format(ag.name))
     return func.HttpResponse(
       body=json.dumps(nic),
       mimetype='application/json',
